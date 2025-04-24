d69bcf0ce4b3a4d9ce99570f3ffdec14026a1956
parth08144 <148989317+parth08144@users.noreply.github.com>
2025-04-24 11:34:00 +0530

    Update ca2 project.py

--- ca2 project.py
+++ ca2 project.py
@@ -14,9 +14,7 @@ df = pd.read_csv("C:/Users/parth/Downloads/INC 5000 Companies 2019.csv")
 
 df.info(), df.head()
 
-# Starter script for Data Cleaning & Preparation
 
-#  Make a copy of the original data
 df_clean = df.copy()
 
 # Clean revenue column - remove "Million", "$", and convert to float (in millions)
@@ -24,14 +22,13 @@ df_clean['revenue'] = df_clean['revenue'].str.replace(' Million', '', regex=Fals
 df_clean['revenue'] = df_clean['revenue'].str.replace('$', '', regex=False)
 df_clean['revenue'] = pd.to_numeric(df_clean['revenue'], errors='coerce')
 
-# For now, we fill missing 'workers' with median value (more robust than mean)
-# Fix for workers
+
 df_clean['workers'] = df_clean['workers'].fillna(df_clean['workers'].median())
 
-# Fix for metro
+
 df_clean['metro'] = df_clean['metro'].fillna('Unknown')
 
-#Create new derived columns
+
 df_clean['company_age_2019'] = 2019 - df_clean['founded']
 
 # Check cleaned data
